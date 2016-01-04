7d24055ea5242f89ccba724aca9b4cb7169b39c7
ejdem <adam_krawiec93@o2.pl>
2016-01-04 23:10:39 +0000

    new navbar in haml in application.hmtl

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   root 'home#index'
+  resources :sessions, only: [:update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
