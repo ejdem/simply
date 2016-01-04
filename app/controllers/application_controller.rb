78db47165268a1e359f4dfa5f8a1fd6840451427
ejdem <adam_krawiec93@o2.pl>
2016-01-04 23:47:30 +0000

    working with cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+  
+  
+  rescue_from CanCan::AccessDenied do |e|
+    flash[:warning] = e.message
+    redirect_to root_path
+  end
+  
+  
+  private
+  
+  def current_user
+    User.new(session[:id])
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104221615) do
+ActiveRecord::Schema.define(version: 20160104233458) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
